d18f833a0a83b27a201d1c834aae9368d4c99a5d
Lucio Barros Jr <barroslucio@live.com>
2015-11-04 10:00:15 -0400

    Alterações na storyboard.

---
+++ publicarro/publicarro/PBCCadastroMotoristaViewController.swift
@@ -0,0 +1,37 @@
+//
+//  PBCCadastroMotoristaViewController.swift
+//  publiCarro
+//
+//  Created by Lúcio Barros on 04/11/15.
+//  Copyright © 2015 tambatech. All rights reserved.
+//
+
+import UIKit
+
+class PBCCadastroMotoristaViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        navigationController?.navigationBar.hidden = false
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
