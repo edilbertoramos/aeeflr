930976d2c3d4f1aff400225037a2d3b6c968999f
eduardapinheiro <eduardapinh@icloud.com>
2015-11-05 10:05:20 -0400

    Criando uma animação que faz o quadrado fazer curvas na view

--- publicarro/PBCAnimacaoViewController.swift
+++ publicarro/PBCAnimacaoViewController.swift
@@ -17,24 +17,42 @@ class PBCAnimacaoViewController: UIViewController {
         
         //colorindo quadrado
         let quadradoColorido = UIView()
+    
         quadradoColorido.backgroundColor = UIColor.greenColor()
         
         // definindo o tamanho do quadrado
-        quadradoColorido.frame = CGRect(x: 0, y: 120, width: 50, height: 50)
+        quadradoColorido.frame = CGRect(x: 55, y: 300, width: 20, height: 20)
         
         
-        self.view.addSubview(quadradoColorido)
+        self.view.addSubview(quadradoColorido) // adicionando o quadrado na view
         
-        // essa animacao move o quadrado da esquerda para direita
+        /* essa animacao move o quadrado da esquerda para direita
         
-        // o método abaixo defini a duração da animação e o estado final da animacao
+        o método abaixo defini a duração da animação e o estado final da animacao
         
         UIView.animateWithDuration(1.0, animations: {quadradoColorido.backgroundColor = UIColor.redColor()
-            quadradoColorido.frame = CGRect(x: 320-50, y: 120, width: 50, height: 50)
-
-
-        // Do any additional setup after loading the view.
+        quadradoColorido.frame = CGRect(x: 320-50, y: 120, width: 50, height: 50)
     })
+        */
+        
+        let path = UIBezierPath()
+        path.moveToPoint(CGPoint(x: 16,y: 239))
+        path.addCurveToPoint(CGPoint(x: 301, y: 239), controlPoint1: CGPoint(x: 136, y: 373), controlPoint2: CGPoint(x: 178, y: 110))
+        
+        // CAKeyframeAnimation anima a posicao dos objetos
+        let anim = CAKeyframeAnimation(keyPath: "position")
+        //definindo o caminho das animacoes
+        anim.path = path.CGPath
+        
+        anim.rotationMode = kCAAnimationRotateAuto
+        anim.repeatCount = Float.infinity
+        anim.duration = 5.0
+        quadradoColorido.layer.addAnimation(anim, forKey: "animate position along path")
+        
+    
+     
+        
+        
 }
 
     override func didReceiveMemoryWarning() {
@@ -42,6 +60,10 @@ class PBCAnimacaoViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func animacaoTapped(sender: AnyObject) {
+        
+        
+    }
     
 
 }
